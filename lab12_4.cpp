70ef2d167a4de60fd25d849fdc97be1c70bd525e
NpS <preeyanan.nps@gmail.com>
2023-12-26 15:27:32 +0700

    12.4

--- lab12_4.cpp
+++ lab12_4.cpp
@@ -2,22 +2,42 @@
 
 using namespace std;
 
-void mySwap(int &,int &); 
+template <typename T>
+void mySwap(T &a, T &b);
+; 
 
 int main(){
-	int x,y;
-	cin >> x >> y;
+	int x, y;
+	string a, b;
+	char p, q;
+	
+	cin >> x >> y >> a >> b >> p >> q;
+	
 	cout << "Before swapping:\n";
 	cout << "x = " << x << ", y = " << y << "\n";
-	mySwap (x,y);
+	mySwap(x,y);
 	cout << "After swapping:\n";
 	cout << "x = " << x << ", y = " << y << "\n";
 	
+	cout << "Before swapping:\n";
+	cout << "a = " << a << ", b = " << b << "\n";
+	mySwap(a,b);
+	cout << "After swapping:\n";
+	cout << "a = " << a << ", b = " << b << "\n";
+	
+	cout << "Before swapping:\n";
+	cout << "p = " << p << ", q = " << q << "\n";
+	mySwap(p,q);
+	cout << "After swapping:\n";
+	cout << "p = " << p << ", q = " << q << "\n";
+
 	return 0;
 }
 
-void mySwap(int &x,int &y){
-    int swap = x;
-    x=y;
-    y=swap;
+template <typename T>
+void mySwap(T &a, T &b) {
+    T temp = a;
+    a = b;
+    b = temp;
 }
+
