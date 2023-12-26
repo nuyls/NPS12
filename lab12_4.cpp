0fa29b136068db00ef2a148109ea949e06c5523f
NpS <preeyanan.nps@gmail.com>
2023-12-26 15:07:04 +0700

    12.3

--- lab12_4.cpp
+++ lab12_4.cpp
@@ -2,31 +2,22 @@
 
 using namespace std;
 
-int main(){
-	int x, y;
-	string a, b;
-	char p, q;
-	
-	cin >> x >> y >> a >> b >> p >> q;
-	
-	cout << "Before swapping:\n";
-	cout << "x = " << x << ", y = " << y << "\n";
-	mySwap(x,y);
-	cout << "After swapping:\n";
-	cout << "x = " << x << ", y = " << y << "\n";
-	
-	cout << "Before swapping:\n";
-	cout << "a = " << a << ", b = " << b << "\n";
-	mySwap(a,b);
-	cout << "After swapping:\n";
-	cout << "a = " << a << ", b = " << b << "\n";
-	
-	cout << "Before swapping:\n";
-	cout << "p = " << p << ", q = " << q << "\n";
-	mySwap(p,q);
-	cout << "After swapping:\n";
-	cout << "p = " << p << ", q = " << q << "\n";
+void mySwap(int &,int &); 
 
+int main(){
+	int x,y;
+	cin >> x >> y;
+	cout << "Before swapping:\n";
+	cout << "x = " << x << ", y = " << y << "\n";
+	mySwap (x,y);
+	cout << "After swapping:\n";
+	cout << "x = " << x << ", y = " << y << "\n";
+	
 	return 0;
 }
 
+void mySwap(int &x,int &y){
+    int swap = x;
+    x=y;
+    y=swap;
+}
